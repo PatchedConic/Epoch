c3acd61e17727678f93244b0f472c9247f4ef892
Isaiah Ware <Patched Conic @ Github>
2025-04-20 18:41:08 -0700

    Began working on check functionality

--- Epoch/GUI.py
+++ Epoch/GUI.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QWidget, QMainWindow, QPushButton, QLabel, QVBoxLayout
+from PyQt6.QtWidgets import QWidget, QMainWindow, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QLineEdit
 from PyQt6.QtCore import Qt
 from Epoch import Generator
 
@@ -9,26 +9,60 @@ class Epoch(QMainWindow):
         self.setWindowTitle("Epoch")
         self.resize(600, 400)
         self.generator = Generator()
+        
+        # self.setCentralWidget(Generate_Single(self.generator))
+        self.setCentralWidget(Check_Single(self.generator))
 
-        self.value = QLabel()
-        self.value.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
-        self.gen_button = QPushButton()
-        self.gen_button.setText("Generate ID")
-        self.gen_button.clicked.connect(self.generate_value)
+class Generate_Single(QWidget):
 
-        self.single_layout = QVBoxLayout()
-        self.single_layout.addWidget(self.value)
-        self.single_layout.addWidget(self.gen_button)
-        self.single = QWidget()
-        self.single.setLayout(self.single_layout)
+    def __init__(self, generator: Generator):
+        super().__init__()
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+        self.generator = generator
 
-        self.setCentralWidget(self.single)
+        self.id_field = QLabel()
+        self.id_field.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
+        self.layout.addWidget(self.id_field)
+
+        self.generate_button = QPushButton()
+        self.generate_button.setText("Generate ID")
+        self.generate_button.clicked.connect(self.generate_value)
+        self.layout.addWidget(self.generate_button)
 
     def generate_value(self):
-        self.value.setText(self.generator.generate()[0])
-# app = QApplication(sys.argv)
+        self.id_field.setText(self.generator.generate()[0])
 
-# window = QMainWindow()
-# window.show()
+class Check_Single(QWidget):
+    
+    class Check_Field(QWidget):
+        def __init__(self):
+            super().__init__()
+            self.layout = QHBoxLayout()
+            self.setLayout(self.layout)
 
-# app.exec()
+            self.check_field = QLineEdit()
+            self.layout.addWidget(self.check_field)
+
+            self.check_status = QLabel()
+            self.layout.addWidget(self.check_status)
+
+
+    def __init__(self, generator: Generator):
+        super().__init__()
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+        self.generator = generator
+
+        self.check_field = self.Check_Field()
+        self.layout.addWidget(self.check_field)
+
+        self.check_button = QPushButton()
+        self.check_button.setText("Check")
+        self.check_button.clicked.connect(self.checkButton)
+        self.layout.addWidget(self.check_button)
+
+    def checkButton(self):
+        status = self.generator.check(self.check_field.check_field.text)
+        self.check_field.check_status.setText(u"Passed u\2713" if status else u"Failed")
+        
